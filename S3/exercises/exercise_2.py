a8cff34514c10d9950f8abd51c3672c5db666ee3
Bogdan <bobo.oprisiu@gmail.com>
2022-10-27 13:39:02 +0300

    S3 finish

--- S3/exercises/exercise_2.py
+++ S3/exercises/exercise_2.py
@@ -1,18 +1,17 @@
 def bigStringSum(s1, s2):
     s = ""
     carry = 0
-    for i in range(len(s1)):
-        s += str((int(s1[i]) + int(s2[i]) + carry) % 10)
+    for i in range(len(s1) - 1, -1, -1):
+        s = str((int(s1[i]) + int(s2[i]) + carry) % 10) + s
 
         if int(s1[i]) + int(s2[i]) + carry > 9:
             carry = 1
         else:
             carry = 0
 
+        print(s)
+
     if carry == 1:
-        if int(s[0]) < 9:
-            s = str(int(s[0]) + carry) + s[0:]
-        else:
-            s = "1" + s
+        s = "1" + s
 
     return s

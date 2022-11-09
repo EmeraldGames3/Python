06e291a8f852ce269ef5d3c4dba050b72f108274
Bogdan <bobo.oprisiu@gmail.com>
2022-11-09 15:53:02 +0200

    L4 ex1 half done

---
+++ L4/ex1/write_turtle.py
@@ -0,0 +1,41 @@
+import turtle
+from ex1.alphabet import *
+
+tur = turtle.Turtle()
+
+ch = {
+    "a": a,
+    "b": b,
+    "c": c,
+    "d": d,
+    "e": e,
+    "f": f,
+    "g": g,
+    "h": h,
+    "i": i,
+    "j": j,
+    "k": k,
+    "l": l,
+    "m": m,
+    "n": n,
+    "o": o,
+    "p": p,
+    "q": q,
+    "r": r,
+    "s": s,
+    "t": t,
+    "u": u,
+    "v": v,
+    "w": w,
+    "x": x,
+    "y": y,
+    "z": z,
+}
+
+
+def writeToTurtle():
+    while True:
+        option = input("Write a letter ").strip()
+        if option in ch:
+            ch[option](tur)
+
--- L4/main.py
+++ L4/main.py
@@ -1,10 +1,12 @@
 from ex3.rock_paper_scissors import playGame
 from ex2.change_words import call_changeWords
+from ex1.write_turtle import writeToTurtle
 
 """
 This dictionary is used to create a menu for the overall homework
 """
 ex = {
+    "1": writeToTurtle,
     "2": call_changeWords,
     "3": playGame,
 }

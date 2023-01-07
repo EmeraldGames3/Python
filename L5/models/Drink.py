27e70b09d1c6442d7845c8c731cd7516c6285225
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-01-07 17:56:00 +0200

    .

--- L5/models/Drink.py
+++ L5/models/Drink.py
@@ -1,7 +1,28 @@
+import hashlib
+
 from models.Dish import Dish
 
 
 class Drink(Dish):
-    def __init__(self, portionsgrose: float, preis: float, alcohoolgehalt: float = 0, id_=-1):
-        super().__init__(portionsgrose, preis, id_)
-        self.__alcohoolgehalt = alcohoolgehalt
+    def __init__(self, id_: int = None, name=None, portion_size: int = None, price: int = None,
+                 alcohol_content: int = None):
+        super().__init__(id_, name, portion_size, price)
+        self.__alcohol_content = alcohol_content
+
+    def __eq__(self, other):
+        return super().__eq__(other) and self.__alcohol_content == other.__alcohol_content
+
+    def __str__(self):
+        return super().__str__() + f", Alkoholgehalt = '{self.__alcohol_content}'"
+
+    def __hash__(self):
+        encoded_str = self.__str__().encode()
+        return hashlib.sha1(encoded_str).hexdigest()
+
+    @property
+    def alcohol_content(self):
+        return self.__alcohol_content
+
+    @alcohol_content.setter
+    def alcohol_content(self, alcohol_content):
+        self.__alcohol_content = alcohol_content
